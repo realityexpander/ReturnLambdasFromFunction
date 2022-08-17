e042644618c4bea70dfaf66525d95e3918de5706
Chris Athanas <realityexpander@gmail.com>
2022-08-17 03:15:33 -0500

    Add interface with default implementations

--- src/main/kotlin/EnumsDemo.kt
+++ src/main/kotlin/EnumsDemo.kt
@@ -1,13 +1,47 @@
 package com.demo.enumsdemo
 
-enum class CoffeeCupSize(val quantity: Int, val price: Float) {
+interface CoffeeCup {
+    fun getCupSizeName(): String = "CoffeeCup"
+    fun getQuantityOfLiquid(): Int = 100
+    fun getSize(): String = "${getQuantityOfLiquid()} ml"
+    fun getInfo(): String = "The ${getCupSizeName()} drink has ${getQuantityOfLiquid()}ml of coffee"
+    fun getPrice(): Float = 1.0f
+    fun calcCost(): (Order) -> Float = { order -> order.numCups * getPrice() }
+}
+
+enum class CoffeeCupSize2(val amtLiquid: Int = 100, val priceOfCup: Float = 1.0f) : CoffeeCup {
+    MEGA(100, 1.5f) {
+        override fun getCupSizeName(): String = name
+        override fun getPrice(): Float = priceOfCup
+        override fun getQuantityOfLiquid(): Int = amtLiquid
+    },
+    GRANDE(50, 1.0f) {
+        override fun getCupSizeName(): String = name
+        override fun getPrice(): Float = priceOfCup
+        override fun getQuantityOfLiquid(): Int = amtLiquid
+    },
+    MEDIUM(30, 0.5f) {
+        override fun getCupSizeName(): String = name
+        override fun getPrice(): Float = priceOfCup
+        override fun getQuantityOfLiquid(): Int = amtLiquid
+    },
+    SMALL(10, 0.25f) {
+        override fun getCupSizeName(): String = name
+        override fun getPrice(): Float = priceOfCup
+        override fun getQuantityOfLiquid(): Int = amtLiquid
+    }
+}
+
+enum class CoffeeCupSize(val amountLiquid: Int, val price: Float) {
     MEGA(100, 1.5f) {
         override fun getSize(): String {
             return "$this.quantity ml"
         }
+
         override fun getInfo(): String {
-            return "The $name drink has $quantity ml of coffee"
+            return "The $name drink has $amountLiquid ml of coffee"
         }
+
         override fun calcCost(): (Order) -> Float {
             return { order -> order.numCups * price }
         }
@@ -16,9 +50,11 @@ enum class CoffeeCupSize(val quantity: Int, val price: Float) {
         override fun getSize(): String {
             return "$this.quantity ml"
         }
+
         override fun getInfo(): String {
-            return "The $name drink has $quantity ml of coffee"
+            return "The $name drink has $amountLiquid ml of coffee"
         }
+
         override fun calcCost(): (Order) -> Float {
             return { order -> order.numCups * price }
         }
@@ -27,9 +63,11 @@ enum class CoffeeCupSize(val quantity: Int, val price: Float) {
         override fun getSize(): String {
             return "$this.quantity ml"
         }
+
         override fun getInfo(): String {
-            return "The $name drink has $quantity ml of coffee"
+            return "The $name drink has $amountLiquid ml of coffee"
         }
+
         override fun calcCost(): (Order) -> Float {
             return { order -> order.numCups * price }
         }
@@ -38,9 +76,11 @@ enum class CoffeeCupSize(val quantity: Int, val price: Float) {
         override fun getSize(): String {
             return "$this.quantity ml"
         }
+
         override fun getInfo(): String {
-            return "The $name drink has $quantity ml of coffee"
+            return "The $name drink has $amountLiquid ml of coffee"
         }
+
         override fun calcCost(): (Order) -> Float {
             return { order -> order.numCups * price }
         }
@@ -53,8 +93,8 @@ enum class CoffeeCupSize(val quantity: Int, val price: Float) {
 
 }
 
-fun priceCalculator(coffeeCupSize: CoffeeCupSize): (Order)->Float {
-    when(coffeeCupSize) {
+fun priceCalculator(coffeeCupSize: CoffeeCupSize): (Order) -> Float {
+    when (coffeeCupSize) {
         CoffeeCupSize.MEGA -> return { order -> order.numCups * 1.5f }
         CoffeeCupSize.GRANDE -> return { order -> order.numCups * 1.0f }
         CoffeeCupSize.MEDIUM -> return { order -> order.numCups * 0.5f }
@@ -94,14 +134,27 @@ fun main(array: Array<String>) {
     val orderOfCoffee2 = OrderOfCoffee(10, CoffeeCupSize.MEGA)
     println("orderOfCoffee2.coffeeCupSize.calcCost() =${orderOfCoffee2.coffeeCupSize.calcCost()(Order(10))}")
 
-    orderArr.forEach {cup ->
+    orderArr.forEach { cup ->
         println("priceCalculator(it.first)(it.second): ${cup.first.getInfo()} = ${priceCalculator(cup.first)(cup.second)}")
     }
 
     var sum = 0f
-    orderArr.forEach {cup ->
+    orderArr.forEach { cup ->
         priceCalculator(cup.first)(cup.second).also { sum += it }
     }
     println("orderArr.sum = $sum")
+
+    val order2 = listOf(
+        CoffeeCupSize2.MEGA,
+        CoffeeCupSize2.GRANDE,
+        CoffeeCupSize2.MEDIUM,
+        CoffeeCupSize2.SMALL
+    )
+
+    order2.forEach {cup ->
+        println("cup2.getInfo() = ${cup.getInfo()}")
+        println("cup2.getPrice() = ${cup.getPrice()}")
+        println("cup2.calcCost() = ${cup.calcCost()(Order(10))}")
+    }
 
 }
